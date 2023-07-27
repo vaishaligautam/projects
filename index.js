2af7553b23f8c38fd8afd397e30df970f1eed655
vaishali <vaishaligautam482@gmail.com>
2023-07-27 11:02:41 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//  $(document).ready(function(){
+// $('.slider').slick({
+//      arrows:false,
+//       dots:true,
+//     appenddots:'slider-dots',
+//      dotsClass:'dots'
+// });
+// // let hamburger = document.querySelector('.hamburger');
+// // let times = document.querySelector('.times');
+// // let mobilenav = document.querySelector('.mobile-nav');
+
+// // hamburger.addEventListener('click', function(){
+// //     mobilenav.classList.add('open');
+// // });
+// // times.addEventListener('click', function(){
+// //     mobilenav.classList.remove==('open');
+// // });
+
+// });
